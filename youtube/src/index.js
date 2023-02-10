b2fbdd19574c21ff7d67feeb43e65feb26e8d2ac
YEZI <art11010@naver.com>
2023-02-11 01:50:13 +0900

    react youtube

---
+++ youtube/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import NotFound from './pages/NotFound';
+import VideoDetail from './pages/VideoDetail';
+import Videos from './pages/Videos';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Videos /> },
+      { path: '/videos', element: <Videos /> },
+      { path: '/videos/:keyword', element: <Videos /> },
+      { path: '/videos/watch/:videoId', element: <VideoDetail /> },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
